3cadfeaa62603a50b1b4bb09a519bf5ccd7baacc
DeveloperRejaul <devrejaul.official@gmail.com>
2024-05-07 11:47:34 +0600

    learn signed and unsigne integer number

---
+++ _3_2_data_type/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    // Integer Types
+    // Length	Signed	    Unsigned
+    // 8-bit	i8	        u8
+    // 16-bit	i16 	    u16
+    // 32-bit	i32	        u32
+    // 64-bit	i64	        u64
+    // 128-bit	i128	    u128
+    // arch	    isize	    usize
+
+   
+    //  *difference between signed and unsigned
+    //  !Signed variable e negative number and positive number store kura jay
+    //  !unsigned variable e sodo positive number store kura jay
+   
+
+    // Example: Signed 
+    let num:i32 = -10;
+    let num2:i32 = 10;
+    println!("{}", num);
+    println!("{}", num2);
+
+    // Example: unsigned 
+    let num3:u32 = 10;
+    println!("{}", num3);
+}
